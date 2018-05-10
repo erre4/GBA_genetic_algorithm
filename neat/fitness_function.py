b0fe8264c1c7b4cfd246814a43224294cbc5787c
Riccardo Viviano <riciric83@gmail.com>
2018-05-10 22:26:23 +0200

    Update fitness_function.py

--- neat/fitness_function.py
+++ neat/fitness_function.py
@@ -29,31 +29,28 @@ def best_genomes(remaining_genomes, species_matrix):
     for i in range(0,len(species_matrix)):
         best_matrix.append([])
         species_matrix[i] = sorted(species_matrix[i], key=operator.attrgetter('fitness'))
-            for j in range(0,len(species_matrix[i]):
+		length = len(species_matrix[i])
+            for j in range(0,length):
                 if counter < remaining_genomes:
-                    best_matrix[i].append(species_matrix[i][j])
+                    best_matrix[i].append(species_matrix[i][length-(j+1)])
                 
                 else:
                     max = 0
                     index = -1
+					min = 9999999
                     for k in range(0,i):
                         if len(best_matrix[k]) > max:
                             max = len(best_matrix[k])
                             index = k
+						elif len(best_matrix[k]) == max:
+							if best_matrix[k][len(best_matrix[k])-1].fitness < best_matrix[index][len(best_matrix[index])-1].fitness:
+								index = k
                             
-                    if(max == 1 and len(best_matrix[i]) == 0):
-                        min = 9999999
-                        index = -1
-                        
-                        for k in range(0,i):
-                            if(best_matrix[k][0].fitness < min):
-                                min = best_matrix[k][0].fitness
-                                index = k
-                                
-                        if(species_matrix[i][j].fitness > min):
-                            best_matrix[k][0] = []
-                            best_matrix[i].append(species_matrix[i][j])
+                    if(max == 1 and len(best_matrix[i]) == 0):        
+                        if(species_matrix[i][length-(j+1)].fitness > min):
+                            best_matrix[index][0] = []
+                            best_matrix[i].append(species_matrix[i][length-(j+1)])
                             
-                    elif(len(best_matrix[i]) < max-2):
-                        best_matrix[k] = best_matrix[1:]
-                        best_matrix[i].append(species_matrix[i][j])
+                    elif(len(best_matrix[i]) < max and species_matrix[i][length-(j+1)].fitness > best_matrix[index][len(best_matrix[index])-1].fitness):
+                        best_matrix[index] = best_matrix[0:len(best_matrix[k])]
+                        best_matrix[i].append(species_matrix[i][length-(j+1)])
